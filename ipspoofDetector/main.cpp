844310ea58659ef0b7bb5f1d8a46ea2dc881a721
乂IBHI <155215525+xibhi@users.noreply.github.com>
2024-11-16 10:41:02 +0530

    Update main.cpp

--- ipspoofDetector/main.cpp
+++ ipspoofDetector/main.cpp
@@ -4,7 +4,22 @@
 #include <algorithm>
 #include <cctype>
 #include <pcap.h>
+
 using namespace std;
+
+string to_opposite_case(const string &input) {
+    string result;
+    for (char ch : input) {
+        if (islower(ch)) {
+            result += toupper(ch);
+        } else if (isupper(ch)) {
+            result += tolower(ch);
+        } else {
+            result += ch;
+        }
+    }
+    return result;
+}
 
 void Interface(){
     system("clear");
@@ -20,63 +35,55 @@ void Interface(){
     cout<<"\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t-by Secure World                                                                                                                                                                                                                     "<<endl;                                                                                                                                                                                            
 }
 
-void print_help(){
-    cout<<"--------------------------------------\n";
-    cout<<"\tTHE AVAILABLE COMMANDS\n";
-    cout<<"--------------------------------------\n";
-    cout<<"help\t\t\t\t:\t\tFor print help."<<endl;
-    cout<<"interfaces\t:\t\tFor listing all network devices from your device."<<endl;
-    cout<<"start ghost :\t\tUsed to capture all packets over internet, and also find the packet ip is spoofed ip or not.\n"<<endl;
+void print_help() {
+    cout << "--------------------------------------\n";
+    cout << "\tTHE AVAILABLE COMMANDS\n";
+    cout << "--------------------------------------\n";
+    cout << "help\t\t\t\t:\t\tFor print help." << endl;
+    cout << "interfaces\t:\t\tFor listing all network devices from your device." << endl;
+    cout << "start ghost :\t\tUsed to capture all packets over internet, and also find the packet IP is spoofed or not.\n" << endl;
 }
 
-void print_all_network_devices(){
-    pcap_if_t *interfaces,*all_interface;
-    char *error;
-    int get_devices = pcap_findalldevs(&all_interface,error);
+void print_all_network_devices() {
+    pcap_if_t *interfaces, *all_interface;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    int get_devices = pcap_findalldevs(&all_interface, errbuf);
 
-    if(get_devices == -1){
-        cout<<"Can\'t list network interface due to"<<error;
+    if (get_devices == -1) {
+        cout << "Can't list network interfaces due to: " << errbuf << endl;
     } else {
-        cout<<"--------------------------------------\n";
-        cout<<"\tTHE AVAILABLE INTERFACES\n";
-        cout<<"--------------------------------------\n";
+        cout << "--------------------------------------\n";
+        cout << "\tTHE AVAILABLE INTERFACES\n";
+        cout << "--------------------------------------\n";
 
-        for(interfaces = all_interface;interfaces;interfaces=interfaces->next){
-            cout<<"Device name :"<<interfaces->name<<endl;
+        for (interfaces = all_interface; interfaces; interfaces = interfaces->next) {
+            cout << "Device name: " << interfaces->name << endl;
         }
     }
-    cout<<'\n';
+    cout << '\n';
 }
 
-int main(){
+int main() {
     string user_cmd;
     Interface();
-    while(true){
-        cout<<"Enter Your Command : ";
-        cin>>user_cmd;
+    while (true) {
+        cout << "Enter Your Command: ";
+        cin >> user_cmd;
 
-        if(user_cmd == "help"){
+        user_cmd = to_opposite_case(user_cmd);
+
+        if (user_cmd == "help") {
             print_help();
-            continue;
-        }
-        else if(user_cmd == "interfaces"){
+        } else if (user_cmd == "interfaces") {
             print_all_network_devices();
-            continue;
-        }
-        else if(user_cmd == "start ghost"){ 
-            //will.
-        }
-        else {
-            cout<<"Error:no commmands found by "<<user_cmd<<endl;
-            continue;
+        } else if (user_cmd == "start ghost") {
+            cout << "Starting ghost functionality (to be implemented)...\n";
+        } else {
+            cout << "Error: No commands found for " << user_cmd << endl;
         }
     }
 }
 
-/**
- * Sibi, below is your task,
- * 
- * TASK:
- * 1.Make the function that's get the string and return the string to opposite case like (lower to upper) or (upper to lower).
- * 2.If you make the function, we use the function for 'user_cmd' variable to make the user input into lower case.
- */
+/*
+Updated by Sibhi
+*/
